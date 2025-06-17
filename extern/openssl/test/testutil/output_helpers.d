e5d15c7b3710baae5ff98ec5372de7a077bd125c
MWednesday <42850043+MWednesday@users.noreply.github.com>
2025-06-17 19:56:37 +0200

    Deleted openssl/test folder to save space

--- extern/openssl/test/testutil/output_helpers.d
+++
@@ -1,7 +0,0 @@
-output_helpers.c
-Note: including file: c:\users\vegeta\documents\visual studio 2017\projects\millionsmaker\extern\openssl\test\testutil\output.h
-Note: including file:  C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\include\stdarg.h
-Note: including file:   C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\include\vcruntime.h
-Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\include\sal.h
-Note: including file:     C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\include\concurrencysal.h
-Note: including file:    C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\include\vadefs.h
